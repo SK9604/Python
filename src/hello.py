81333557f6bbfe5ee90c2f7fad1e2bb3a559874a
SK9604 <tjsrud428@gmail.com>
2021-05-17 23:11:39 +0900

    문자열 결합

--- src/hello.py
+++ src/hello.py
@@ -1,3 +1,5 @@
-msg = "Hello World"
-print(msg.upper())
-print(msg.lower())
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print("Hello, " + full_name.title() + "!")
