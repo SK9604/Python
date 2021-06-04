79abc4980d84070b32f818f58b00ea7a1e79edd6
SK9604 <tjsrud428@gmail.com>
2021-06-04 22:21:54 +0900

    range()

--- src/hello.py
+++ src/hello.py
@@ -1,27 +1,2 @@
-bicycles = ['cannondale', 'trek', 'redline', 'specialized']
-print(bicycles[1])
-print(bicycles[-1])
-bicycles[1] = 'hello'
-print(bicycles[1])
-bicycles.append('bye')
-print(bicycles[-1])
-print(bicycles)
-pop_bicycles = bicycles.pop()
-
-print(pop_bicycles)
-print(bicycles)
-
-dele = 'cannondale'
-bicycles.remove(dele)
-bicycles.append('do')
-print(bicycles)
-bicycles.sort()
-print(bicycles)
-bicycles.sort(reverse=True)
-print(bicycles)
-print(sorted(bicycles))
-print(len(bicycles))
-
-for bicycle in bicycles:
-    print(bicycle.title() + ", that was trick!")
-print("end of for")
+for value in range(1,5):
+    print(value)
