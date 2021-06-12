d3beac4a9d7c99235e0c01d6ebc1884547c0a5ba
SK9604 <tjsrud428@gmail.com>
2021-06-12 23:15:15 +0900

    slice loop

--- src/hello.py
+++ src/hello.py
@@ -1,3 +1,6 @@
 squares = [value**2 for value in range(1,11)]
 print(squares[:4])
 print(squares[2:])
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+for players in players[:3]:
+    print(players.title())
