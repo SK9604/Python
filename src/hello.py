c8817bb4df175bbe2beddf4e6a3d00825523e56f
SK9604 <tjsrud428@gmail.com>
2021-06-14 21:34:59 +0900

    list append

--- src/hello.py
+++ src/hello.py
@@ -6,6 +6,8 @@ for players in players[:3]:
     print(players.title())
 my_foods = ['pizza','falafel','carrot cake']
 friend_foods = my_foods[:]
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
 print("My favorite foods are : ")
 print(my_foods)
 print("\nMy friend's favorite foods are : ")
