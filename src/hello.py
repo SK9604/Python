052bd1e73c7e827bb674e5737c0a59c93869833a
SK9604 <tjsrud428@gmail.com>
2021-06-15 23:31:46 +0900

    tuple

--- src/hello.py
+++ src/hello.py
@@ -1,14 +1,3 @@
-squares = [value**2 for value in range(1,11)]
-print(squares[:4])
-print(squares[2:])
-players = ['charles', 'martina', 'michael', 'florence', 'eli']
-for players in players[:3]:
-    print(players.title())
-my_foods = ['pizza','falafel','carrot cake']
-friend_foods = my_foods[:]
-my_foods.append('cannoli')
-friend_foods.append('ice cream')
-print("My favorite foods are : ")
-print(my_foods)
-print("\nMy friend's favorite foods are : ")
-print(friend_foods)
+dimensions=(200,5)
+print(dimensions[0])
+print(dimensions[1])
