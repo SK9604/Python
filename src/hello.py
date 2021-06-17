b355ce6426fc9f744bc636b503c35a3588a6d863
SK9604 <tjsrud428@gmail.com>
2021-06-17 22:20:24 +0900

    덮어쓰기

--- src/hello.py
+++ src/hello.py
@@ -1,5 +1,10 @@
 dimensions=(200,5)
 print(dimensions[0])
 print(dimensions[1])
+print("Original")
+for dimension in dimensions:
+    print(dimension)
+dimensions=(400,100)
+print("Modified")
 for dimension in dimensions:
     print(dimension)
